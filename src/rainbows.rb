d46c421bfdc4136b30370a3a9882eae7d47993aa
Lito Nicolai <lito.nicolai@gmail.com>
2015-07-30 21:36:30 -0700

    Hue to RGB implementation

--- src/rainbows.rb
+++ src/rainbows.rb
@@ -1,19 +1,62 @@
+# TODO(Lito): respect min and max
+# (By wrapping each fn?)
+
 class Rainbow
   def greyscale degree
     brightness_unit = degree/360.0
-    brightness = brightness_unit*255.0 # Scale back to RGB
+    brightness = (brightness_unit*255.0).floor # Scale back to RGB
     [brightness, brightness, brightness]
   end
 
-  def hue
+  ##
+  # The full RGB spectrum
+  #
+  def hue degree
+    main_color = 1 * 255 # Let chroma (saturation * brightness) always == 1
+    second_strongest_color = ((1 - (degree/60.0 % 2 - 1).abs) * 255).floor
+
+    case degree
+    when 0..60
+      [main_color, second_strongest_color, 0]
+    when 61..120
+      [second_strongest_color, main_color, 0]
+    when 121..180
+      [0, main_color, second_strongest_color]
+    when 181..240
+      [0, second_strongest_color, main_color]
+    when 241..300
+      [second_strongest_color, 0, main_color]
+    when 301..360
+      [main_color, 0, second_strongest_color]
+    end
   end
 
-  def cubehelix
+  ##
+  # Spectrum with linearly increaing brightness
+  #
+  def cubehelix degree
   end
 
-  def perceptual
+  ##
+  # Spectrum with constant brightness
+  #
+  def perceptual degree
   end
 
-  def jet
+  ##
+  #
+  #
+  def jet degree # NOTE(Lito): Do we really need this?
+  end
+
+  ##
+  #
+  def parula degree
+  end
+
+  ##
+  # Blue-green-yellow-red spectrum for for simulations
+  #
+  def nico degree
   end
 end
